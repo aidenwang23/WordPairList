0d6c16a8f93f6e8d5c4c7287c1343cf84faa2574
aidenwang23 <148901698+aidenwang23@users.noreply.github.com>
2025-04-29 00:15:24 +0000

    Complete

---
+++ WordPairList.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+public class WordPairList {
+    /** The list of word pairs, initialized by the constructor. */
+    private ArrayList<WordPair> allPairs;
+
+    /**
+     * Constructs a WordPairList object as described in part (a).
+     * Precondition: words.length >= 2
+     */
+    public WordPairList(String[] words) {
+        allPairs = new ArrayList<WordPair>();
+        for(int i=0; i<words.length-1; i++){
+            for(int ii=i+1; ii<words.length; ii++){
+                WordPair w = new WordPair(words[i], words[ii]);
+                allPairs.add(w);
+            }
+        }
+    }
+
+    /**
+     * Returns the number of matches as described in part (b).
+     */
+    public int numMatches() {
+        int matches = 0;
+        for(WordPair w : allPairs){
+            if(w.getFirst().equals(w.getSecond())) matches++;
+        }
+        return matches;
+    }
+
+    public ArrayList<WordPair> getAllPairs(){
+        return allPairs;
+    }
+}
